33eb19a00c79a13165458856f565122f0556d610
Lene Naevdal <l.naevdal@gmail.com>
2013-02-22 21:07:36 +1100

    The first commit of the first project at pilsprog melbourne!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pilsprogmissioncontrol::Application.config.secret_token = '775f0cf708ef40fdbf7e0fee7e1a002a661c5d0e10b2c68b0abc2d02a07db86778937cb8bef614323ae523967d22e63f1c66ba6acf2d280c333db409fa844f5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pilsprogmissioncontrol::Application.config.session_store :cookie_store, key: '_pilsprogmissioncontrol_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pilsprogmissioncontrol::Application.config.session_store :active_record_store
